34a9d4aa8ab536cb78dfc6e2ca60a6e142ed115a
Swetha Repakula <srepaku@us.ibm.com>
2018-02-12 16:27:46 -0800

    Use gorilla json rpc library
       - getCode, call, sendTransaction implement ethereum json rpc API
       - getTransactionReceipt still is incomplete

--- ethserver/codec.go
+++ ethserver/codec.go
@@ -1,64 +1,37 @@
 package ethserver
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"net/http"
-	"net/rpc"
-	"net/rpc/jsonrpc"
 	"strings"
+
+	"github.com/gorilla/rpc"
+	"github.com/gorilla/rpc/json"
 )
 
 type rpcCodec struct {
-	codec rpc.ServerCodec
+	codec *json.Codec
 }
 
-type httpConn struct {
-	in  io.Reader
-	out io.Writer
+type codecRequest struct {
+	rpc.CodecRequest
 }
 
-func (c *httpConn) Read(data []byte) (n int, err error)  { return c.in.Read(data) }
-func (c *httpConn) Write(data []byte) (n int, err error) { return c.out.Write(data) }
-func (c *httpConn) Close() error                         { return nil }
-
-func NewRPCCodec(r *http.Request, w http.ResponseWriter) rpc.ServerCodec {
-	return &rpcCodec{
-		codec: jsonrpc.NewServerCodec(&httpConn{in: r.Body, out: w}),
-	}
+func NewRPCCodec() rpc.Codec {
+	codec := json.NewCodec()
+	return &rpcCodec{codec: codec}
 }
 
-func (c *rpcCodec) ReadRequestHeader(req *rpc.Request) error {
-	err := c.codec.ReadRequestHeader(req)
+func (c *rpcCodec) NewRequest(r *http.Request) rpc.CodecRequest {
+	req := c.codec.NewRequest(r)
+	return &codecRequest{req}
+}
+
+func (r *codecRequest) Method() (string, error) {
+	m, err := r.CodecRequest.Method()
 	if err != nil {
-		return err
+		return "", err
 	}
-	serviceMethod := strings.Split(req.ServiceMethod, "_")
-	service := "EthRPCService"
-	var method string
-
-	switch serviceMethod[0] {
-	case "web3":
-		method = strings.Title(serviceMethod[len(serviceMethod)-1])
-	case "eth":
-		method = strings.Title(serviceMethod[len(serviceMethod)-1])
-	default:
-		return errors.New("Service not found")
-	}
-	req.ServiceMethod = fmt.Sprintf("%s.%s", service, method)
-
-	return nil
-}
-
-func (c *rpcCodec) ReadRequestBody(body interface{}) error {
-	return c.codec.ReadRequestBody(body)
-}
-
-func (c *rpcCodec) WriteResponse(res *rpc.Response, body interface{}) error {
-	return c.codec.WriteResponse(res, body)
-}
-
-func (c *rpcCodec) Close() error {
-	return c.codec.Close()
+	method := strings.Split(m, "_")
+	return fmt.Sprintf("%s.%s", method[0], strings.Title(method[1])), nil
 }
--- ethserver/server.go
+++ ethserver/server.go
@@ -1,16 +1,20 @@
 package ethserver
 
 import (
+	"encoding/hex"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
-	"net/rpc"
+	"strconv"
 
 	"github.com/gogo/protobuf/proto"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/rpc"
 	"github.com/hyperledger/fabric-sdk-go/api/apitxn"
 	"github.com/hyperledger/fabric-sdk-go/pkg/config"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
+	"github.com/hyperledger/fabric/protos/common"
 	"github.com/hyperledger/fabric/protos/peer"
 )
 
@@ -19,15 +23,33 @@ type EthRPCService struct {
 }
 
 type EthService interface {
-	GetCode(*GetCodeArgs, *string) error
+	GetCode(*http.Request, *DataParam, *string) error
+	Call(*http.Request, *Params, *string) error
+	SendTransaction(*http.Request, *Params, *string) error
+	GetTransactionReceipt(*http.Request, *DataParam, *TxReceipt) error
 }
 
 type ethRPCService struct {
 	sdk *fabsdk.FabricSDK
 }
 
-type EthRPCArgs struct{}
-type GetCodeArgs string
+type DataParam string
+type Params struct {
+	From     string
+	To       string
+	Gas      string
+	GasPrice string
+	Value    string
+	Data     string
+	Nonce    string
+}
+
+type TxReceipt struct {
+	TransactionHash string
+	BlockHash       string
+	BlockNumber     string
+	ContractAddress string
+}
 
 type EthServer struct {
 	Server   *rpc.Server
@@ -54,7 +76,8 @@ func NewEthServer(eth EthService) *EthServer {
 	server := rpc.NewServer()
 
 	ethService := EthRPCService{eth}
-	server.Register(ethService)
+	server.RegisterCodec(NewRPCCodec(), "application/json")
+	server.RegisterService(ethService, "eth")
 
 	return &EthServer{
 		Server: server,
@@ -62,29 +85,13 @@ func NewEthServer(eth EthService) *EthServer {
 }
 
 func (s *EthServer) Start(port int) {
-	l, e := net.Listen("tcp", fmt.Sprintf(":%d", port))
-	if e != nil {
-		log.Fatal("listen error:", e)
-	}
-	s.listener = l
+	r := mux.NewRouter()
+	r.Handle("/", s.Server)
 
-	http.Serve(l, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		rpcCodec := NewRPCCodec(r, w)
-		w.Header().Set("Content-type", "application/json")
-		err := s.Server.ServeRequest(rpcCodec)
-		if err != nil {
-			errMsg := fmt.Sprintf("Error while serving JSON request, %s", err.Error())
-			http.Error(w, errMsg, 500)
-		}
-		w.WriteHeader(200)
-	}))
+	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
 }
 
-func (s *EthServer) Stop() {
-	s.listener.Close()
-}
-
-func (req *ethRPCService) GetCode(args *GetCodeArgs, reply *string) error {
+func (req *ethRPCService) GetCode(r *http.Request, args *DataParam, reply *string) error {
 
 	chClient, err := req.sdk.NewChannelClient(channelID, defaultUser)
 	if err != nil {
@@ -93,39 +100,30 @@ func (req *ethRPCService) GetCode(args *GetCodeArgs, reply *string) error {
 
 	defer chClient.Close()
 
-	queryArgs := [][]byte{[]byte(channelID), []byte(*args)}
+	queryArgs := [][]byte{[]byte(*args)}
 
-	value, err := Query(chClient, "lscc", "getdepspec", queryArgs)
+	value, err := Query(chClient, "evmscc", "getCode", queryArgs)
 	if err != nil {
 		fmt.Printf("Failed to query: %s\n", err)
 	}
-
-	cds := &peer.ChaincodeDeploymentSpec{}
-	err = proto.Unmarshal(value, cds)
-	if err != nil {
-		log.Fatalf("Failed to unmarshal code: %s", err)
-	}
-
-	*reply = string(cds.CodePackage)
+	*reply = string(value)
 
 	return nil
 }
 
-func (req *ethRPCService) GetBlockByHash(args *GetCodeArgs, reply *string) error {
+func (req *ethRPCService) Call(r *http.Request, params *Params, reply *string) error {
 
 	chClient, err := req.sdk.NewChannelClient(channelID, defaultUser)
 	if err != nil {
-		log.Panic("error creating client", err)
+		return err
 	}
-
 	defer chClient.Close()
 
-	var value []byte
+	args := [][]byte{[]byte(params.Data)}
 
-	queryArgs := [][]byte{[]byte(channelID), []byte(*args)}
-	value, err = Query(chClient, "qscc", "GetBlockByHash", queryArgs)
+	value, err := Query(chClient, "evmscc", params.To, args)
 	if err != nil {
-		log.Fatalf("Failed to query qscc, function: GetBlockByHash", err)
+		return err
 	}
 
 	*reply = string(value)
@@ -133,24 +131,82 @@ func (req *ethRPCService) GetBlockByHash(args *GetCodeArgs, reply *string) error
 	return nil
 }
 
-func (req *ethRPCService) GetBlockByNumber(args *GetCodeArgs, reply *string) error {
+func (req *ethRPCService) SendTransaction(r *http.Request, params *Params, reply *string) error {
 
 	chClient, err := req.sdk.NewChannelClient(channelID, defaultUser)
 	if err != nil {
-		log.Panic("error creating client", err)
+		return err
 	}
-
 	defer chClient.Close()
 
-	var value []byte
+	txReq := apitxn.ExecuteTxRequest{
+		ChaincodeID: "evmscc",
+		Fcn:         params.To,
+		Args:        [][]byte{[]byte(params.Data)},
+	}
 
-	queryArgs := [][]byte{[]byte(channelID), []byte(*args)}
-	value, err = Query(chClient, "qscc", "GetBlockByNumber", queryArgs)
-	log.Fatalf("Failed to query qscc, function: GetBlockByNumber", err)
+	//Return only the transaction ID
+	//Maybe change to an async transaction
+	_, txID, err := chClient.ExecuteTx(txReq)
+	if err != nil {
+		return err
+	}
 
-	*reply = string(value)
+	*reply = txID.ID
 
 	return nil
+}
+
+//TODO: Return only the transaction result in the Contract Address spot.
+func (req *ethRPCService) GetTransactionReceipt(r *http.Request, param *DataParam, reply *TxReceipt) error {
+
+	chClient, err := req.sdk.NewChannelClient(channelID, defaultUser)
+
+	args := [][]byte{[]byte(channelID), []byte(*param)}
+
+	t, err := Query(chClient, "qscc", "GetTransactionByID", args)
+	if err != nil {
+		return err
+	}
+
+	tx := &peer.ProcessedTransaction{}
+	err = proto.Unmarshal(t, tx)
+	if err != nil {
+		return err
+	}
+
+	b, err := Query(chClient, "qscc", "GetBlockByTxID", args)
+	if err != nil {
+		return err
+	}
+
+	block := &common.Block{}
+	err = proto.Unmarshal(b, block)
+	if err != nil {
+		return err
+	}
+
+	blkHeader := block.GetHeader()
+
+	p := tx.GetTransactionEnvelope().GetPayload()
+	payload := &common.Payload{}
+	err = proto.Unmarshal(p, payload)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("PAYLOAD HEADERS: %+v\n\n", payload.GetHeader())
+
+	// have to figure out when to pass in the contract address or not
+	*reply = TxReceipt{
+		TransactionHash: string(*param),
+		BlockHash:       hex.EncodeToString(blkHeader.Hash()),
+		BlockNumber:     strconv.FormatUint(blkHeader.GetNumber(), 10),
+		ContractAddress: string(payload.GetData()),
+	}
+
+	return nil
+
 }
 
 func Query(chClient apitxn.ChannelClient, chaincodeID string, function string, queryArgs [][]byte) ([]byte, error) {
@@ -160,5 +216,4 @@ func Query(chClient apitxn.ChannelClient, chaincodeID string, function string, q
 		Fcn:         function,
 		Args:        queryArgs,
 	})
-
 }
